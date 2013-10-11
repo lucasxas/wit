fb7237e957a3136cbdc6ffec9006408f4797bd84
lucasxas <lucasxas@gmail.com>
2013-10-11 15:14:58 -0300

    Now wit is able to move the watermark in 5 different positions.

--- wit.py
+++ wit.py
@@ -2,26 +2,41 @@ import Image, ImageEnhance, os
 from os.path import join
 
 def batch():
-	infolder = os.getcwd()
-	watermark = str(raw_input('Type the watermark\'s file name (including the .png or .jpg): '))
-	if not watermark: watermark = "watermark.png"
-	outfolder = os.getcwd()+"/output/"
-	if not os.path.exists(outfolder): os.makedirs(outfolder)
-	mark_dir = infolder+"/"+watermark
-	mark = Image.open(mark_dir)
-	for root, dir, files in os.walk(infolder):
-		if watermark in files: files.remove(watermark)
-		for name in files:        
-			try:
-				im = Image.open(join(root, name))
-				if im.mode != 'RGBA':
-					im = im.convert('RGBA')
-				layer = Image.new('RGBA', im.size, (0,0,0,0))
-				position = (im.size[0]-(mark.size[0]*2), im.size[1]-(mark.size[1]*2))
-				layer.paste(mark, position)
-				Image.composite(layer, im, layer).save( join(outfolder, name))
-			except Exception, (msg):
-				print ""
+    infolder = os.getcwd()
+    p = str(raw_input('Type the watermark\'s position (1 to 5, default: 5): '))
+    if not p: p = 5
+    p = int(p)
+    watermark = str(raw_input('Type the watermark\'s file name (including the .png or .jpg): '))
+    if not watermark: watermark = "watermark.png"
+    outfolder = os.getcwd()+"/output/"
+    mark_dir = infolder+"/"+watermark
+    mark = Image.open(mark_dir)
+    for root, dir, files in os.walk(infolder):
+        if watermark in files: files.remove(watermark)
+        for name in files: 
+            try:
+                im = Image.open(join(root, name))
+                layer = Image.new('RGBA', im.size, (0,0,0,0))               
+                if im.mode != 'RGBA':
+                    im = im.convert('RGBA')
+                position = getPos(im, mark, p)
+                layer.paste(mark, position)
+                if not os.path.exists(outfolder): os.makedirs(outfolder)
+                Image.composite(layer, im, layer).save( join(outfolder, name))
+            except Exception, (msg):
+                print ""
  
+def getPos(ima, mar, pos):
+    if pos == 1:
+        return (50, 50)
+    elif pos == 2:
+        return (ima.size[0]-(mar.size[0]) -50, 50)
+    elif pos == 3:
+        return ((ima.size[0]-mar.size[0])/2 -25, (ima.size[1]-mar.size[1])/2 -25)
+    elif pos == 4:
+        return (50, ima.size[1]-(mar.size[1])-50)
+    else:
+        return (ima.size[0] - mar.size[0] - 50, ima.size[1]-mar.size[1] - 50)
+        
 if __name__ == '__main__':
     batch()
